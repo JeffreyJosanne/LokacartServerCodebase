faf904d8263e859c89fdf6c38efc20e440a28e38
Siddharth Ramachandran <siddharth.ramachandran2012@vit.ac.in>
2016-02-03 16:09:29 +0530

    Added API to edit and view profile settings

--- src/main/java/app/business/controllers/rest/SettingsRestController.java
+++ src/main/java/app/business/controllers/rest/SettingsRestController.java
@@ -6,17 +6,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import app.business.services.BillLayoutSettingsService;
 import app.business.services.OrganizationService;
+import app.business.services.UserPhoneNumberService;
+import app.business.services.UserService;
 import app.entities.BillLayoutSettings;
 import app.entities.Organization;
+import app.entities.User;
+import app.entities.UserPhoneNumber;
 
 @Controller
 @RequestMapping("/api/{org}")
@@ -28,22 +34,30 @@ public class SettingsRestController {
 	@Autowired
 	BillLayoutSettingsService billLayoutSettingsService;
 	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	UserPhoneNumberService userPhoneNumberService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	
 	@RequestMapping(value="/billsettingsupdate", method=RequestMethod.POST, produces = "application/json")
 	@PreAuthorize("hasRole('ADMIN'+#org)")
 	@Transactional
 	public @ResponseBody String changeBillSettings(@PathVariable String org, @RequestBody String requestBody) {
 		JSONObject jsonResponseObject = new JSONObject();
-		String abbr = null, newName = null, newAddress = null, newContact = null, newHeader = null, newFooter = null;
+		String newName = null, newAddress = null, newContact = null, newHeader = null, newFooter = null;
 		try{
 			JSONObject object = new JSONObject(requestBody);
-			abbr = object.getString("orgabbr");
 			newName = object.getString("newname");
 			newAddress = object.getString("newaddress");
 			newContact = object.getString("newcontact");
 			newHeader = object.getString("newheader");
 			newFooter = object.getString("newfooter");
-			Organization organization = organizationService.getOrganizationByAbbreviation(abbr);
+			Organization organization = organizationService.getOrganizationByAbbreviation(org);
 			BillLayoutSettings billLayoutSettings = billLayoutSettingsService.getBillLayoutSettingsByOrganization(organization);
 			organization.setName(newName);
 			organization.setAddress(newAddress);
@@ -74,10 +88,9 @@ public class SettingsRestController {
 	
 	@RequestMapping(value="/billsettingsview", method=RequestMethod.GET, produces = "application/json")
 	@PreAuthorize("hasRole('ADMIN'+#org)")
-	@Transactional
+
 	public @ResponseBody String viewBillSettings(@PathVariable String org) {
 		JSONObject jsonResponseObject = new JSONObject();
-		String abbr = null;
 		try{
 			
 			Organization organization = organizationService.getOrganizationByAbbreviation(org);
@@ -106,8 +119,88 @@ public class SettingsRestController {
 		
 	}
 	
+	@RequestMapping(value="/profilesettingsview", method=RequestMethod.GET, produces = "application/json")
+	@PreAuthorize("hasRole('ADMIN'+#org)")
 	
 	
+	public @ResponseBody String viewProfileSettings(@PathVariable String org, @RequestParam String number) {
+		JSONObject jsonResponseObject = new JSONObject();
+		try{
+			UserPhoneNumber userPhoneNumber = userPhoneNumberService.getUserPhoneNumber(number);
+			User user = userPhoneNumber.getUser();
+			jsonResponseObject.put("name", user.getName());
+			jsonResponseObject.put("email",user.getEmail());
+			jsonResponseObject.put("phonenumber",userPhoneNumber.getPhoneNumber());
+			jsonResponseObject.put("address", user.getAddress());
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			try {
+				jsonResponseObject.put("response", "failure");
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			}
+			return jsonResponseObject.toString();
+		}
+		
+		try {
+			jsonResponseObject.put("response", "success");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return jsonResponseObject.toString();
+	}
+	
+	
+	
+	@RequestMapping(value="/profilesettingsupdate", method=RequestMethod.POST, produces = "application/json")
+	@PreAuthorize("hasRole('ADMIN'+#org)")
+	@Transactional
+	
+	public @ResponseBody String changeProfileSettings(@PathVariable String org, @RequestBody String requestBody) {
+		JSONObject jsonResponseObject = new JSONObject();
+		String phonenumber = null, email =null, name = null, password = null, address = null, newNumber = null;
+		try{
+			JSONObject object = new JSONObject(requestBody);
+			phonenumber = object.getString("phonenumber");
+			newNumber = object.getString("newnumber");
+			email = object.getString("email");
+			name = object.getString("name");
+			address = object.getString("address");
+			UserPhoneNumber userPhoneNumber = userPhoneNumberService.getUserPhoneNumber(phonenumber);
+			User user = userPhoneNumber.getUser();
+			try{
+				password = object.getString("password");
+				user.setSha256Password(passwordEncoder.encode(password));
+			}
+			catch(Exception e){
+				//do nothing, as no password update requested
+			}
+			user.setName(name);
+			user.setEmail(email);
+			user.setAddress(address);
+			userPhoneNumber.setPhoneNumber(newNumber);
+			userPhoneNumberService.addUserPhoneNumber(userPhoneNumber);
+			userService.addUser(user);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			try {
+				jsonResponseObject.put("response", "failure");
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			}
+			return jsonResponseObject.toString();
+		}
+	
+		try {
+			jsonResponseObject.put("response", "Successfully updated");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return jsonResponseObject.toString();
+	}
 	
 }
 
